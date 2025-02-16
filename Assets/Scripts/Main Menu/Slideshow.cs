2a4a5881ba275ea7c0b96b5ab5052f2f61a003a4
Kawu <138832632+Kawudesu@users.noreply.github.com>
2025-02-16 22:39:13 +0800

    Updated Slideshow Controller
    Updated Slideshow Controller

--- Assets/Scripts/Main Menu/Slideshow.cs
+++ Assets/Scripts/Main Menu/Slideshow.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SlideshowController : MonoBehaviour
 {
     public Image[] slides;
-    public float slideDuration = 5f; 
+    public TMP_Text slideText;
+    public string[] slideTexts = new string[5]; // 5 slides with unique text
+    public float slideDuration = 5f;
+    public float typingSpeed = 0.05f;
+
     private int currentSlideIndex = 0;
 
     void Start()
     {
+        if (slides.Length != 5 || slideTexts.Length != 5)
+        {
+            Debug.LogError("There must be exactly 5 slides and 5 text entries.");
+            return;
+        }
+
         foreach (Image slide in slides)
         {
             slide.gameObject.SetActive(false);
@@ -22,21 +33,27 @@ public class SlideshowController : MonoBehaviour
 
     IEnumerator PlaySlideshow()
     {
-        while (true)
+        for (int i = 0; i < 5; i++)
         {
-            yield return new WaitForSeconds(slideDuration);
+            slideText.text = "";
+            yield return StartCoroutine(TypeText(slideTexts[i]));
+            yield return new WaitForSeconds(3f);
 
-            slides[currentSlideIndex].gameObject.SetActive(false);
+            slides[i].gameObject.SetActive(false);
 
-            currentSlideIndex = (currentSlideIndex + 1) % slides.Length;
-
-            slides[currentSlideIndex].gameObject.SetActive(true);
-
-            if (currentSlideIndex == slides.Length - 1)
+            if (i < 4)
             {
-                yield return new WaitForSeconds(slideDuration);
-                break;
+                slides[i + 1].gameObject.SetActive(true);
             }
+        }
+    }
+
+    IEnumerator TypeText(string text)
+    {
+        foreach (char letter in text.ToCharArray())
+        {
+            slideText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
         }
     }
 }

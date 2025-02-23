333f36feae05b635f9c825c98cdfe181d064bec3
Kawu <138832632+kawudiv@users.noreply.github.com>
2025-02-23 17:41:06 +0800

    Improved Lore Slideshow
    Improved Lore Slideshow

--- Assets/Scripts/Main Menu/Slideshow.cs
+++ Assets/Scripts/Main Menu/Slideshow.cs
@@ -1,24 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SlideshowController : MonoBehaviour
 {
     public Image[] slides;
     public TMP_Text slideText;
     public string[] slideTexts = new string[5]; 
-    public float slideDuration = 5f;
     public float typingSpeed = 0.05f;
+    public TMP_Text tapToContinueText;
+    public string nextSceneName;
 
     private int currentSlideIndex = 0;
+    private bool isTyping = false;
 
     void Start()
     {
+        tapToContinueText.gameObject.SetActive(false);
+        StartCoroutine(BlinkTapToContinue());
+        
         if (slides.Length != 5 || slideTexts.Length != 5)
         {
-            Debug.LogError("There must be exactly 5 slides and 5 text entries.");
+            Debug.LogError("There must be 5 slides");
             return;
         }
 
@@ -28,24 +33,26 @@ public class SlideshowController : MonoBehaviour
         }
         slides[0].gameObject.SetActive(true);
 
-        StartCoroutine(PlaySlideshow());
+        StartCoroutine(ShowSlide(currentSlideIndex));
     }
 
-    IEnumerator PlaySlideshow()
+    void Update()
     {
-        for (int i = 0; i < 5; i++)
+        if (Input.GetMouseButtonDown(0) && !isTyping)
         {
-            slideText.text = "";
-            yield return StartCoroutine(TypeText(slideTexts[i]));
-            yield return new WaitForSeconds(3f);
-
-            slides[i].gameObject.SetActive(false);
-
-            if (i < 4)
-            {
-                slides[i + 1].gameObject.SetActive(true);
-            }
+            NextSlide();
         }
+    }
+
+    IEnumerator ShowSlide(int index)
+    {
+        isTyping = true;
+        slideText.text = "";
+        tapToContinueText.gameObject.SetActive(false);
+        yield return StartCoroutine(TypeText(slideTexts[index]));
+        isTyping = false;
+        tapToContinueText.gameObject.SetActive(true);
+        StartCoroutine(BlinkTapToContinue());
     }
 
     IEnumerator TypeText(string text)
@@ -55,5 +62,36 @@ public class SlideshowController : MonoBehaviour
             slideText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
+    }
+
+    IEnumerator BlinkTapToContinue()
+    {
+        while (tapToContinueText.gameObject.activeSelf)
+        {
+            tapToContinueText.enabled = !tapToContinueText.enabled;
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    void NextSlide()
+    {
+        tapToContinueText.gameObject.SetActive(false);
+        slides[currentSlideIndex].gameObject.SetActive(false);
+        currentSlideIndex++;
+
+        if (currentSlideIndex < slides.Length)
+        {
+            slides[currentSlideIndex].gameObject.SetActive(true);
+            StartCoroutine(ShowSlide(currentSlideIndex));
+        }
+        else
+        {
+            GoToNextScene();
+        }
+    }
+
+    void GoToNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }

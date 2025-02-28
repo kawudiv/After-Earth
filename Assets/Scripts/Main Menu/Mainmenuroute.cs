1acb4b20d74e9454d3b796580209c09355f543a1
Kawu <138832632+kawudiv@users.noreply.github.com>
2025-03-01 01:49:36 +0800

    New Main Menu Design
    New Main Menu Design

--- Assets/Scripts/Main Menu/Mainmenuroute.cs
+++ Assets/Scripts/Main Menu/Mainmenuroute.cs
@@ -2,15 +2,57 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class Mainmenuroute : MonoBehaviour
 {
 
-    public void PlayGame ()
+    public TextMeshProUGUI tapanywheretoplay;
+    public int nextSceneIndex = 1;
+    public float fadeDuration = 1.5f;
+
+    private void Start()
     {
-        SceneManager.LoadSceneAsync(1);
+        if(tapanywheretoplay != null)
+        {
+            StartCoroutine(FadeText());
+        }   
     }
+
+    void Update()
+    {
+        if (Input.anyKeyDown || Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }   
+    }
+
+
+    private IEnumerator FadeText()
+    {
+        while (true)
+        {
+            for (float t = 0; t < fadeDuration; t+= Time.deltaTime)
+            {
+                float alpha = Mathf.Lerp(1, 0, t / fadeDuration);
+                tapanywheretoplay.color = new Color(tapanywheretoplay.color.r, tapanywheretoplay.color.g, tapanywheretoplay.color.b, alpha);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+
+            for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+            {
+                float alpha = Mathf.Lerp(0, 1, t / fadeDuration);
+                tapanywheretoplay.color = new Color(tapanywheretoplay.color.r, tapanywheretoplay.color.g, tapanywheretoplay.color.b, alpha);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
     public void Continue()
     {
         SceneManager.LoadSceneAsync(2);
